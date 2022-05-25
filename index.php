3e147bf5c379b6b35709d32ed1819b1f5fadc080
webdesignercrazyworld <105739222+webdesignercrazyworld@users.noreply.github.com>
2022-05-25 09:14:19 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,177 @@
+<html>
+<head>
+<title></title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- font -->
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+#like a:hover {
+color:red;
+}
+
+</style>
+
+
+</head>
+
+<body class="">
+
+<div class="container bg-dark">
+
+<ul class="nav nav-bar nav-justified">
+<li class="nav-item"><a href="home.html" class="nav-link text-warning">Home</a></li>
+<li class="nav-item"><a href="about.html" class="nav-link text-primary">About us</a></li>
+<li class="nav-item"><a href="service.html" class="nav-link text-info">Service</a></li>
+<li class="nav-item"><a href="login.html" class="nav-link text-success">Contact us</a></li>
+<li class="nav-item"><a href="register.html" class="nav-link text-success">register</a></li>
+</ul>
+</div><br>
+
+<div class="carousel slide float-right w-75 mr-5" id="m" data-ride="carousel">
+<div class="carousel-inner">
+
+<div class="carousel-item active ">
+<img src="8.jpg" alt="image" class="mx-auto d-block" style="width:600px;" "height:600px;">
+</div>
+
+<div class="carousel-item">
+<img src="11.jpg" alt="image" class="mx-auto d-block" style="width:600px;" "height:600px;">
+</div>
+
+<div class="carousel-item">
+<img src="12.jpg" alt="image" class="mx-auto d-block" style="width:600px;" "height:600px;">
+</div>
+
+<div class="carousel-caption">
+<button class="btn btn-success text-white btn-outline-primary" href="cargo.html">click here</button>
+<h2 class="text-info">our company give a service to develope <br>a responsive web designing</h2>
+<p>do you want to online selling your product.</p>
+</div>
+
+<ul class="carousel-indicators">
+<li data-target="#m" data-slide-to="0" class="active"></li>
+<li data-target="#m" data-slide-to="1"></li>
+<li data-target="#m" data-slide-to="2"></li>
+</ul>
+
+<a href="#m" data-slide="prev" class="carousel-control-prev">
+<span class="carousel-control-prev-icon"></span> </a>
+
+<a href="#m" data-slide="next" class="carousel-control-next">
+<span class="carousel-control-next-icon"></span> </a>
+
+</div>
+</div>
+
+<div class="w-100 bg-info">
+<ul class="nav nav-bar mr-4 mt-2 float-left d-block bg-secondary">
+<li class="nav-item dropdown">
+<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">designs</a>
+<div class="dropdown-menu">
+<a href="#" class="dropdown-item">web 1</a>
+<a href="#" class="dropdown-item">web 2</a>
+<a href="#" class="dropdown-item">web 3</a>
+</div>
+</li>
+
+<li class="nav-item dropdown">
+<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Product</a>
+<div class="dropdown-menu">
+<a href="shoes.html" class="dropdown-item">Shoes</a>
+<a href="cargo.html" class="dropdown-item">cargo</a>
+<a href="" class="dropdown-item">formal pants</a>
+</div>
+</li>
+
+<li class="nav-item dropdown">
+<a href="" class="nav-link dropdown-toggle" data-toggle="dropdown">section</a>
+<div class="dropdown-menu">
+<a href="sections.html" class="dropdown-item">section1</a>
+<a href="" class="dropdown-item">Section2</a>
+<a href="" class="dropdown-item">Section3</a>
+</div>
+</li>
+
+<li class="nav-item dropdown ">
+<a href="" class="nav-link dropdown-toggle" data-toggle="dropdown">section1</a>
+<div class="dropdown-menu">
+<a href="sections.html"  class="dropdown-item text-warning">noti</a>
+<a href="sections.html" class="dropdown-item text-warning">noti</a>
+<a href="sections.html" class="dropdown-item text-warning">noti</a>
+</div>
+</li>
+</ul>
+</div>
+
+<form class="m-1">
+<img src="03.jpg">
+<div >WelcomE To Our website
+
+<h class="text-danger">Uploading</h>
+<div input type="range" class="form-control-range w-75">
+<input type="file" class="form-control-file border">
+</div>
+</div>
+
+<div class="w-100 mt-5 text-center">
+<h3 class="text-white text-center"><span class="text-success">G </span><span class="text-primary">o o o o o o o o</span><span class="text-success"> g</span> <span class="text-primary">l<span clas="text-warning"> e</span>  </h3>
+<ul class="pagination pagination-lg text-primary justify-content-center mb-3 pb-1">
+<li class="page-item active"><a href="index.php" class="page-link">Previous</a></li>
+<li class="page-item"><a href="cargo.html" class="page-link">1</a></li>
+<li class="page-item"><a href="shoes.html" class="page-link">2</a></li>
+<li class="page-item"><a href="sections.html" class="page-link">3</a></li>
+<li class="page-item"><a href="service.html" class="page-link">4</a></li>
+<li class="page-item"><a href="login.html" class="page-link">5</a></li>
+<li class="page-item"><a href="About.html" class="page-link">Next</a></li>
+
+</ul>
+</div>
+
+</form>
+
+<div class=" bg-dark m-auto mx-5 text-center px-5 w-75 h-25 d-flex">
+<div class="mr-5 mx-5 px-5 d-flex">
+<div class="">
+<ul class="nav nav-bar col-lg-6 d-block mx-5 px-5 ml-5 mr-5">
+<h>Service</h>
+<li class="footer-item"><a class="footer-link">Cargo</a></li>
+<li class="footer-item"><a class="footer-link">Shoes</a></li>
+<li class="footer-item"><a class="footer-link">Cargo</a></li>
+</ul>
+</div>
+
+<div>
+<ul class="nav nav-bar col-6 d-block px-5 ml-5 mr-5">
+<h>Product</h>
+<li class="footer-item"><a class="footer-link">Shoes</a></li>
+<li class="footer-item"><a class="footer-link">Cargo</a></li>
+<li class="footer-item"><a class="footer-link">Shoes</a></li>
+</ul>
+</div>
+
+<div>
+<ul class="nav nav-bar col-lg-6 d-block px-5 ml-5 mr-5">
+<h>Shopping</h>
+<li class="footer-item"><a class="cargo.html">Cargo</a></li>
+<li class="footer-item"><a class="footer-link">Shoes</a></li>
+<li class="footer-item"><a class="footer-link">Cargo</a></li>
+</ul>
+</div>
+</div>
+
+</div>
+</body>
+</html>
